90a06c366d736c40e25c5c4d54fe41557571f475
chris <cakellum@bsu.edu>
2019-09-20 19:58:22 -0400

    basic structure update to the logic section

---
+++ src/main/java/logic/InputInterpreter.java
@@ -0,0 +1,19 @@
+package logic;
+
+public class InputInterpreter {
+
+
+
+    public void number1(String number1){
+
+    }
+
+    public void number2(String number2){
+
+    }
+
+    public void operand(String operand){
+
+    }
+
+}
