a7a4b728dd67daaf79bd737c972ec606f11b62c8
Michael Gallegos <magallegos56@gmail.com>
2019-09-20 15:43:13 -0400

    Added Maven Dependencies

---
+++ src/main/java/gui/CalculatorFace.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CalculatorFace extends Application{
+
+
+    public void start(Stage stage) throws Exception {
+
+    }
+}
