5570d5eafc0ff6c444f8a1ec3ecbc21ee2ff7aef
chris <cakellum@bsu.edu>
2019-09-20 20:27:06 -0400

    basic structure update to the logic section

--- src/main/java/logic/InputInterpreter.java
+++ src/main/java/logic/InputInterpreter.java
@@ -2,18 +2,35 @@ package logic;
 
 public class InputInterpreter {
 
+    //make private variables to hold until equal sign is pressed
 
-
-    public void number1(String number1){
-
+    public void number1Catch(String number1){
+        //set a private variable
     }
 
-    public void number2(String number2){
-
+    public void number2Catch(String number2){
+        //set a private variable
     }
 
-    public void operand(String operand){
-
+    public String answerThrower(){
+        // enter the number/s in the appropriate Operators.method
+        return null;
     }
+
+    public void operandCatch(String operand){
+
+        if ("add".equals(operand)) {
+        } else if ("sub".equals(operand)) {
+        } else if("mul".equals(operand)){
+        } else if("div".equals(operand)){
+        } else if("pow".equals(operand)){
+        } else if("sqrt".equals(operand)){
+        }else{
+            System.err.println("Internal error");
+            System.exit(1);
+        }
+    }
+
+
 
 }
--- src/main/java/logic/Operators.java
+++ src/main/java/logic/Operators.java
@@ -1,6 +1,4 @@
 package logic;
 
 public class Operators {
-
-
 }
