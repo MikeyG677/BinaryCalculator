d0bcb421774074d29e69b85a14ea5895339a9582
chris <cakellum@bsu.edu>
2019-09-21 13:14:34 -0400

    interpreter inplace

--- src/main/java/logic/InputInterpreter.java
+++ src/main/java/logic/InputInterpreter.java
@@ -2,29 +2,50 @@ package logic;
 
 public class InputInterpreter {
 
-    //make private variables to hold until equal sign is pressed
+    private long number1 =0,number2 =0;
+    private String operator =null;
 
     public void number1Catch(String number1){
-        //set a private variable
+        this.number1 = Long.parseLong(number1, 2);
     }
 
     public void number2Catch(String number2){
-        //set a private variable
+        this.number2 = Long.parseLong(number2,2);
     }
 
     public String answerThrower(){
-        // enter the number/s in the appropriate Operators.method
-        return null;
+        long ans =0;
+
+        if ("add".equals(this.operator)) {
+            ans = Operators.add(this.number1,this.number2);
+        } else if ("sub".equals(this.operator)) {
+            ans = Operators.subtract(this.number1,this.number2);
+        } else if("mul".equals(this.operator)) {
+            ans = Operators.multiply(this.number1,this.number2);
+        } else if("div".equals(this.operator)) {
+            ans = Operators.divide(this.number1,this.number2);
+        } else {
+            System.err.println("Internal error");
+            System.exit(1);
+        }
+
+        return Long.toBinaryString(ans);
     }
 
     public void operandCatch(String operand){
 
         if ("add".equals(operand)) {
+            this.operator = operand;
         } else if ("sub".equals(operand)) {
+            this.operator = operand;
         } else if("mul".equals(operand)){
+            this.operator = operand;
         } else if("div".equals(operand)){
+            this.operator = operand;
         } else if("pow".equals(operand)){
+            this.operator = operand;
         } else if("sqrt".equals(operand)){
+            this.operator = operand;
         }else{
             System.err.println("Internal error");
             System.exit(1);
